a522bbb4e6b7d76758013f3f6253362f35764e81
Catalina\mramu <Catalina-1>
2018-11-15 12:22:43 +0530

    local commit

---
+++ src/com/mindtree/devopsPackage/Demo1.java
@@ -0,0 +1,15 @@
+/**
+ * Author : M1040615
+ * Create date : Nov 15, 2018 | 12:21:10 PM
+ * Modified : 12:21:10 PM
+ */
+package com.mindtree.devopsPackage;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!!");
+
+	}
+
+}
